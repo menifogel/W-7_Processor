c9a2452abe0d26c6514fc14e5ea04be0a44861fa
meni-bird <141713777+meni-bird@users.noreply.github.com>
2025-08-07 19:23:48 +0300

    chnge from localhost to env

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 function App() {
   const [file, setFile] = useState(null);
